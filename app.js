a05a9f49dbd7756190dab0fbe3b6cd9d3b1f8633
Max Girkens <maxgirkens@gmail.com>
2023-11-24 12:14:15 +0100

    calendar 2024

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { jsPDF } from 'jspdf'
 import 'jspdf-autotable'
 import * as fs from 'fs'
 
-const year = 2023
+const year = 2024
 
 const doc = new jsPDF()
 
@@ -13,9 +13,8 @@ let lastWeekNumber = null
 
 const dates = {}
 while (date.getFullYear() < year + 1) {
-  const days = Math.floor((date - startDate) / (24 * 60 * 60 * 1000))
+  const days = Math.floor((date - startDate) / (24 * 60 * 60 * 1000)) + 1
   const weekNumber = Math.ceil(days / 7)
-
   const day = date.getDay() === 0 ? 7 : date.getDay()
 
   //init new week
@@ -47,15 +46,22 @@ while (weeks[weeks.length - 1].length < 8) {
   weeks[weeks.length - 1].push('')
 }
 
+const defaultCellWidth = 25
+
 doc.autoTable({
   styles: { fontSize: 3.75, halign: 'left', textColor: 'black' },
-  headStyles: { halign: 'left', fontStyle: 'bold', cellWidth: 'auto', fillColor: null },
+  headStyles: { halign: 'left', fontStyle: 'bold', fillColor: null },
   bodyStyles: { textColor: 'darkgray', lineWidth: { top: 0.1, left: 0.1 } },
   columnStyles: {
     lineWidth: { top: 0.1 },
     0: { fontStyle: 'bold', halign: 'right', cellWidth: 7, textColor: 'black', lineWidth: { top: 0.1, left: 0 } },
-    6: { fillColor: '#fcf7dd' },
-    7: { fillColor: '#f9f0ca' },
+    1: { cellWidth: defaultCellWidth },
+    2: { cellWidth: defaultCellWidth },
+    3: { cellWidth: defaultCellWidth },
+    4: { cellWidth: defaultCellWidth },
+    5: { cellWidth: defaultCellWidth },
+    6: { fillColor: '#fcf7dd', cellWidth: defaultCellWidth },
+    7: { fillColor: '#f9f0ca', cellWidth: defaultCellWidth },
   },
   margin: { top: 10, bottom: 10 },
   head: [['', 'MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO']],
